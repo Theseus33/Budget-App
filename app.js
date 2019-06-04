868429aeed82e9a97f969f207fe7847b089fb8a2
Rana Bhattacharyya <ranab33@gmail.com>
2019-06-04 15:51:05 -0400

    set up event listeners

--- app.js
+++ app.js
@@ -13,7 +13,28 @@ var UIController = (function () {
 //where we control what happens on each event and delegate tasks
 var controller = (function (budgetCtrl, UICtrl) {
 
-  document.querySelector('.add__btn').addEventListener('click', function () {
-    console.log('button ws clicked')
+  var ctrlAddItem = function () {
+    //1. Get the field input data
+
+    //2. Add the item to the budget controller
+
+    //3. Add the item to the UI
+
+    //4. Calculate the budget
+
+    //5. Display the budget on the UI
+    console.log('Works!');
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  //add event listener to global event
+  document.addEventListener('keypress', function (event) {
+
+    if (event.keyCode === 13 || event.which === 13) {
+
+    }
   });
+
+
 })(budgetController, UIController);
