c13d3fcb0e22e86d40c63fbdf14779ed5a6f5937
Rana Bhattacharyya <ranab33@gmail.com>
2019-06-06 18:05:37 -0400

    add item prevents 0 and NaN uodates

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ var UIController = (function () {
         //will be either 'inc' or 'exp'
         type: document.querySelector(DOMstrings.inputType).value,
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
     },
 
@@ -145,26 +145,35 @@ var controller = (function (budgetCtrl, UICtrl) {
     });
   }
 
+  var updateBudget = function () {
+    //1. Calculate the budget
 
+    //5. Calculate the budget
+
+    //6. Display the budget on the UI
+
+  };
 
   var ctrlAddItem = function () {
     var input, newItem;
 
     //1. Get the field input data
     input = UICtrl.getInput();
-    console.log(input);
 
-    //2. Add the item to the budget controller which accepts 3 params type, description and value
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
 
-    //3. Add the item to the UI
-    UICtrl.addListItem(newItem, input.type);
+      //2. Add the item to the budget controller which accepts 3 params type, description and value
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-    //4. Clear the fields
-    UICtrl.clearFields();
-    //5. Calculate the budget
+      //3. Add the item to the UI
+      UICtrl.addListItem(newItem, input.type);
 
-    //6. Display the budget on the UI
+      //4. Clear the fields
+      UICtrl.clearFields();
+
+      //5. Calculate and update budget
+      updateBudget();
+    }
 
   };
 
