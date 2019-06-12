3f2115c5082359c7e99a754d3d449b11f3fdad8d
Rana Bhattacharyya <ranab33@gmail.com>
2019-06-12 18:06:29 -0400

    setup percentages controller

--- app.js
+++ app.js
@@ -220,6 +220,16 @@ var controller = (function (budgetCtrl, UICtrl) {
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+  };
+
+  //update the percentage shown function and place it in add and remove
+
+  var updatePercentages = () {
+    //1. Calculate percentages
+
+    //2. Read percemtages from the budget controller
+
+    //3. Update the UI with the new percentages
   }
 
   var updateBudget = function () {
@@ -250,6 +260,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
       //5. Calculate and update budget
       updateBudget();
+
+      //6. Calculate and update percentages
+      updatePercentages();
     }
 
   };
@@ -274,6 +287,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
       //3. Update and show the new budget
       updateBudget();
+
+      //4. Calculate and update percentages
+      updatePercentages();
     }
   };
 
