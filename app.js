3d503f55c0fae87c6715cbc579cfbb72dfff2824
Rana Bhattacharyya <ranab33@gmail.com>
2019-06-11 22:50:04 -0400

    UI reflects deleted item updated budget total

--- app.js
+++ app.js
@@ -156,6 +156,12 @@ var UIController = (function () {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    deleteListItem: function (selectorID) {
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
+
+    },
+
     clearFields: function () {
       var fields, fieldsArr;
       //will return a list instead of an array
@@ -264,8 +270,10 @@ var controller = (function (budgetCtrl, UICtrl) {
       //1. delete the item from the data structure
       budgetCtrl.deleteItem(type, id);
       //2. Delete the item from the UI
+      UICtrl.deleteListItem(itemID);
 
       //3. Update and show the new budget
+      updateBudget();
     }
   };
 
