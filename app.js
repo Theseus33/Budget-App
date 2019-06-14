469c8d9c356f210f2554ce3853a1bb50dce2b59e
Rana Bhattacharyya <ranab33@gmail.com>
2019-06-14 19:13:04 -0400

    date display

--- app.js
+++ app.js
@@ -149,7 +149,8 @@ var UIController = (function () {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
 
   };
 
@@ -267,6 +268,17 @@ var UIController = (function () {
 
     },
 
+    displayMonth: function () {
+      var now, year, month, months, day;
+      var now = new Date();
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      month = now.getMonth();
+
+      year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+    },
+
     //3.exposing private DOMstrings to public
     getDOMstrings: function () {
       return DOMstrings;
@@ -369,6 +381,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
   return {
     init: function () {
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
